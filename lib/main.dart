160cfc049265b92880ccd28fd08bf42c281598af
Carlos Cazelatto Junior <carlosjunior1983@gmail.com>
2021-10-24 18:33:33 -0300

    Nagigation in forms

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_crud/models/user.dart';
 import 'package:flutter_crud/provider/users.dart';
+import 'package:flutter_crud/routes/app_routes.dart';
+import 'package:flutter_crud/views/user_form.dart';
 import 'package:flutter_crud/views/user_list.dart';
 import 'package:provider/provider.dart';
 
@@ -20,12 +22,14 @@ class MyApp extends StatelessWidget {
         )
       ],
       child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: UserList(),
-      ),
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          routes: {
+            AppRoutes.HOME: (_) => UserList(),
+            AppRoutes.USER_FORM: (_) => UserForm()
+          }),
     );
   }
 }
--- lib/views/user_list.dart
+++ lib/views/user_list.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_crud/components/user_tile.dart';
 import 'package:flutter_crud/data/dummy_users.dart';
+import 'package:flutter_crud/models/user.dart';
 import 'package:flutter_crud/provider/users.dart';
+import 'package:flutter_crud/routes/app_routes.dart';
 import 'package:provider/provider.dart';
 
 class UserList extends StatelessWidget {
@@ -11,7 +13,11 @@ class UserList extends StatelessWidget {
 
     return Scaffold(
         appBar: AppBar(title: Text('Lista de Usuários :)'), actions: <Widget>[
-          IconButton(onPressed: () {}, icon: Icon(Icons.add))
+          IconButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed(AppRoutes.USER_FORM);
+              },
+              icon: Icon(Icons.add))
         ]),
         body: ListView.builder(
           itemCount: users.count,
